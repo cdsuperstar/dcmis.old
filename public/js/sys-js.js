84a128a93174ee390b999896e50d53deb5005351
vagrant <vagrant@homestead>
2015-07-21 14:09:05 +0000

    wxb commit

---
+++ public/js/sys-js.js
@@ -0,0 +1,197 @@
+$(function(){
+	InitLeftMenu();
+	tabClose();
+	tabCloseEven();
+	clockon();
+});
+
+//初始化左侧
+function InitLeftMenu() {
+
+    $(".easyui-accordion").empty();
+    var menulist = "";
+   
+    $.each(_menus.menus, function(i, n) {
+        menulist += '<div title="'+n.menuname+'"  icon="'+n.icon+'" style="overflow:auto;">';
+		menulist += '<ul>';
+        $.each(n.menus, function(j, o) {
+			menulist += '<li><div><a target="mainFrame" href="' + o.url + '" ><span class="icon '+o.icon+'" ></span>' + o.menuname + '</a></div></li> ';
+        });
+        menulist += '</ul></div>';
+    });
+    
+	$(".easyui-accordion").append(menulist);
+	
+	$('.easyui-accordion li a').click(function(){
+		var tabTitle = $(this).text();
+		var url = $(this).attr("href");
+		addTab(tabTitle,url);
+		$('.easyui-accordion li div').removeClass("selected");
+		$(this).parent().addClass("selected");
+	}).hover(function(){
+		$(this).parent().addClass("hover");
+	},function(){
+		$(this).parent().removeClass("hover");
+	});
+    console.info($(".easyui-accordion").accordion());
+}
+
+function addTab(subtitle,url){
+	if(!$('#tabs').tabs('exists',subtitle)){
+		$('#tabs').tabs('add',{
+			title:subtitle,
+			content:createFrame(url),
+			closable:true,
+			width:$('#mainPanle').width()-10,
+			height:$('#mainPanle').height()-26
+		});
+	}else{
+		$('#tabs').tabs('select',subtitle);
+	}
+	tabClose();
+}
+
+function createFrame(url)
+{
+	var s = '<iframe name="mainFrame" scrolling="auto" frameborder="0"  src="'+url+'" style="width:100%;height:100%;"></iframe>';
+	return s;
+}
+
+function tabClose()
+{
+	/*双击关闭TAB选项卡*/
+	$(".tabs-inner").dblclick(function(){
+		var subtitle = $(this).children("span").text();
+		if(subtitle!='我的工作台')
+            $('#tabs').tabs('close', subtitle);
+	});
+
+	$(".tabs-inner").bind('contextmenu',function(e){
+		$('#mm').menu('show', {
+			left: e.pageX,
+			top: e.pageY,
+		});
+		
+		var subtitle =$(this).children("span").text();
+		$('#mm').data("currtab",subtitle);
+		
+		return false;
+	});
+}
+//绑定右键菜单事件
+function tabCloseEven()
+{
+	//关闭当前
+	$('#mm-tabclose').click(function(){
+		var currtab_title = $('#mm').data("currtab");
+        if(currtab_title!='我的工作台')
+            $('#tabs').tabs('close', currtab_title);
+	});
+	//全部关闭
+	$('#mm-tabcloseall').click(function(){
+		$('.tabs-inner span').each(function(i,n){
+			var t = $(n).text();
+            if(t!='我的工作台')
+                $('#tabs').tabs('close', t);
+		});	
+	});
+	//关闭除当前之外的TAB
+	$('#mm-tabcloseother').click(function(){
+		var currtab_title = $('#mm').data("currtab");
+		$('.tabs-inner span').each(function(i,n){
+			var t = $(n).text();
+			if(t!=currtab_title && t!="我的工作台")
+				$('#tabs').tabs('close',t);
+		});	
+	});
+	//关闭当前右侧的TAB
+	$('#mm-tabcloseright').click(function(){
+		var nextall = $('.tabs-selected').nextAll();
+		if(nextall.length==0){
+			msgShow('后边没有啦~~');
+			return false;
+		}
+		nextall.each(function(i,n){
+			var t=$('a:eq(0) span',$(n)).text();
+            if(t!='我的工作台')
+                $('#tabs').tabs('close',t);
+		});
+		return false;
+	});
+	//关闭当前左侧的TAB
+	$('#mm-tabcloseleft').click(function(){
+		var prevall = $('.tabs-selected').prevAll();
+		if(prevall.length==0){
+			msgShow('到头了，前边没有啦~~');
+			return false;
+		}
+		prevall.each(function(i,n){
+			var t=$('a:eq(0) span',$(n)).text();
+            if(t!='我的工作台')
+                $('#tabs').tabs('close',t);
+		});
+		return false;
+	});
+
+	//退出
+	$("#mm-exit").click(function(){
+		$('#mm').menu('hide');
+	});
+}
+
+function clockon() {
+    var now = new Date();
+    var year = now.getFullYear(); //getFullYear getYear
+    var month = now.getMonth();
+    var date = now.getDate();
+    var day = now.getDay();
+    var hour = now.getHours();
+    var minu = now.getMinutes();
+    var sec = now.getSeconds();
+    var week;
+    month = month + 1;
+    if (month < 10) month = "0" + month;
+    if (date < 10) date = "0" + date;
+    if (hour < 10) hour = "0" + hour;
+    if (minu < 10) minu = "0" + minu;
+    if (sec < 10) sec = "0" + sec;
+    var arr_week = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+    week = arr_week[day];
+    var time = "";
+    time = year + "年" + month + "月" + date + "日" + " " + hour + ":" + minu + ":" + sec + " " + week;
+
+    $("#bgclock").html(time);
+
+    var timer = setTimeout("clockon()", 200);
+}
+function msgShow(Msg){
+	 var options = { 
+	    title: "操作提示", 
+	    msg: "<font color=red>"+Msg+"</font>", 
+	    showType: 'slide', 
+	    timeout: 5000 
+	}; 
+	$.messager.show(options); 
+}
+
+/**
+ * 更换EasyUI主题的方法
+ * @param themeName
+ * 主题名称
+ */
+changeTheme = function(themeName) {
+	var $easyuiTheme = $('#easyuiTheme');
+	var url = $easyuiTheme.attr('href');
+	var href = url.substring(0, url.indexOf('themes')) + 'themes/' + themeName + '/easyui.css';
+	$easyuiTheme.attr('href', href);
+	var $iframe = $('iframe');
+	if ($iframe.length > 0) {
+		for ( var i = 0; i < $iframe.length; i++) {
+			var ifr = $iframe[i];
+			$(ifr).contents().find('#easyuiTheme').attr('href', href);
+		}
+	}
+	$.cookie('easyuiThemeName', themeName, {
+		expires : 7
+	});
+};
