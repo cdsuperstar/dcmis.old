01109e6d49d1584f6189aca487063cc08e4ac1ec
cdsuperstar <181977814@qq.com>
2015-07-25 14:03:49 +0000

    tans login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 <!-- BEGIN HEAD -->
 <head>
 <meta charset="utf-8"/>
-<title>Metronic | Login Options - Login Form 1</title>
+<title>澳斯特 | 教学管理系统</title>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
 <meta http-equiv="Content-type" content="text/html; charset=utf-8">
@@ -43,7 +43,7 @@
 <!-- BEGIN LOGO -->
 <div class="logo">
 	<a href="#">
-	<img src="/metronic/assets/admin/layout/img/logo-big.png" alt=""/>
+	<img src="/metronic/assets/admin/layout/img/logo-big.png" alt="澳斯特教育"/>
 	</a>
 </div>
 <!-- END LOGO -->
@@ -52,7 +52,7 @@
 	<!-- BEGIN LOGIN FORM -->
 	<form class="login-form" method="post" action="{{ url('/auth/login') }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-		<h3 class="form-title">Sign In</h3>
+		<h3 class="form-title">用户登录</h3>
 		<div class="alert alert-danger display-hide">
 			<button class="close" data-close="alert"></button>
 			<span>
@@ -60,21 +60,21 @@
 		</div>
 		<div class="form-group">
 			<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-			<label class="control-label visible-ie8 visible-ie9">Username</label>
-			<input class="form-control form-control-solid placeholder-no-fix" type="email" autocomplete="off" placeholder="Email" name="email" value="{{ old('email') }}"/>
+			<label class="control-label visible-ie8 visible-ie9">用户名</label>
+			<input class="form-control form-control-solid placeholder-no-fix" type="email" autocomplete="off" placeholder="邮箱" name="email" value="{{ old('email') }}"/>
 		</div>
 		<div class="form-group">
 			<label class="control-label visible-ie8 visible-ie9">Password</label>
-			<input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="Password" name="password"/>
+			<input class="form-control form-control-solid placeholder-no-fix" type="password" autocomplete="off" placeholder="密码" name="password"/>
 		</div>
 		<div class="form-actions">
-			<button type="submit" class="btn btn-success uppercase">Login</button>
+			<button type="submit" class="btn btn-success uppercase"> - 登 录 - </button>
 			<label class="rememberme check">
-			<input type="checkbox" name="remember" value="1"/>Remember </label>
-			<a href="javascript:;" id="forget-password" class="forget-password">Forgot Password?</a>
+			<input type="checkbox" name="remember" value="1"/>下次自动登录 </label>
+			<a href="javascript:;" id="forget-password" class="forget-password">忘记密码 ?</a>
 		</div>
 		<div class="login-options">
-			<h4>Or login with</h4>
+			<h4>可以使用其他方式登录</h4>
 			<ul class="social-icons">
 				<li>
 					<a class="social-icon-color facebook" data-original-title="facebook" href="javascript:;"></a>
@@ -90,96 +90,26 @@
 				</li>
 			</ul>
 		</div>
-		<div class="create-account">
-			<p>
-				<a href="javascript:;" id="register-btn" class="uppercase">Create an account</a>
-			</p>
-		</div>
 	</form>
 	<!-- END LOGIN FORM -->
 	<!-- BEGIN FORGOT PASSWORD FORM -->
 	<form class="forget-form" action="index.html" method="post">
-		<h3>Forget Password ?</h3>
+		<h3>忘记密码 ?</h3>
 		<p>
-			 Enter your e-mail address below to reset your password.
+			 请输入注册邮箱用来重置密码.
 		</p>
 		<div class="form-group">
-			<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Email" name="email"/>
+			<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="邮箱" name="email"/>
 		</div>
 		<div class="form-actions">
-			<button type="button" id="back-btn" class="btn btn-default">Back</button>
-			<button type="submit" class="btn btn-success uppercase pull-right">Submit</button>
+			<button type="button" id="back-btn" class="btn btn-default"> - 后 退 - </button>
+			<button type="submit" class="btn btn-success uppercase pull-right"> - 提 交 - </button>
 		</div>
 	</form>
 	<!-- END FORGOT PASSWORD FORM -->
-	<!-- BEGIN REGISTRATION FORM -->
-	<form class="register-form" action="index.html" method="post">
-		<h3>Sign Up</h3>
-		<p class="hint">
-			 Enter your personal details below:
-		</p>
-		<div class="form-group">
-			<label class="control-label visible-ie8 visible-ie9">Full Name</label>
-			<input class="form-control placeholder-no-fix" type="text" placeholder="Full Name" name="fullname"/>
-		</div>
-		<div class="form-group">
-			<!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
-			<label class="control-label visible-ie8 visible-ie9">Email</label>
-			<input class="form-control placeholder-no-fix" type="text" placeholder="Email" name="email"/>
-		</div>
-		<div class="form-group">
-			<label class="control-label visible-ie8 visible-ie9">Address</label>
-			<input class="form-control placeholder-no-fix" type="text" placeholder="Address" name="address"/>
-		</div>
-		<div class="form-group">
-			<label class="control-label visible-ie8 visible-ie9">City/Town</label>
-			<input class="form-control placeholder-no-fix" type="text" placeholder="City/Town" name="city"/>
-		</div>
-		<div class="form-group">
-			<label class="control-label visible-ie8 visible-ie9">Country</label>
-			<select name="country" class="form-control">
-				<option value="">Country</option>
-				<option value="WF">Wallis and Futuna Islands</option>
-				<option value="EH">Western Sahara</option>
-				<option value="YE">Yemen</option>
-				<option value="ZM">Zambia</option>
-				<option value="ZW">Zimbabwe</option>
-			</select>
-		</div>
-		<p class="hint">
-			 Enter your account details below:
-		</p>
-		<div class="form-group">
-			<label class="control-label visible-ie8 visible-ie9">Username</label>
-			<input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username"/>
-		</div>
-		<div class="form-group">
-			<label class="control-label visible-ie8 visible-ie9">Password</label>
-			<input class="form-control placeholder-no-fix" type="password" autocomplete="off" id="register_password" placeholder="Password" name="password"/>
-		</div>
-		<div class="form-group">
-			<label class="control-label visible-ie8 visible-ie9">Re-type Your Password</label>
-			<input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Re-type Your Password" name="rpassword"/>
-		</div>
-		<div class="form-group margin-top-20 margin-bottom-20">
-			<label class="check">
-			<input type="checkbox" name="tnc"/> I agree to the <a href="javascript:;">
-			Terms of Service </a>
-			& <a href="javascript:;">
-			Privacy Policy </a>
-			</label>
-			<div id="register_tnc_error">
-			</div>
-		</div>
-		<div class="form-actions">
-			<button type="button" id="register-back-btn" class="btn btn-default">Back</button>
-			<button type="submit" id="register-submit-btn" class="btn btn-success uppercase pull-right">Submit</button>
-		</div>
-	</form>
-	<!-- END REGISTRATION FORM -->
 </div>
 <div class="copyright">
-	 2014 © Metronic. Admin Dashboard Template.
+	 2015 © xx科技公司. Admin Dashboard Template.
 </div>
 <!-- END LOGIN -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
