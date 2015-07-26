955f98f443be7a678f9f73ce538ae5ccfe62a9fb
cdsuperstar <181977814@qq.com>
2015-07-26 09:38:00 +0000

    modi home.blade

--- resources/views/definitionframe/pagehead.php
+++ resources/views/definitionframe/pagehead.php
@@ -46,7 +46,7 @@ License: You must have a valid license purchased only from themeforest(the above
     <link href="/metronic/assets/admin/layout/css/themes/darkblue.css" rel="stylesheet" type="text/css" id="style_color"/>
     <link href="/metronic/assets/admin/layout/css/custom.css" rel="stylesheet" type="text/css"/>
     <!-- END THEME STYLES -->
-    <link rel="shortcut icon" href="/metronic/assets/image/favicon.ico"/>
+    <link rel="shortcut icon" href="favicon.ico"/>
 </head>
 <!-- END HEAD -->
 <!-- BEGIN BODY -->
