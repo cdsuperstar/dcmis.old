db70d9739321859b6fb132229cc6179833bf1392
cdsuperstar <181977814@qq.com>
2015-08-12 11:18:31 +0800

    add dcmodels ,update sys-model,update routes

---
+++ app/models/dcmodel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class dcmodel
+ * @package App\models
+ */
+class dcmodel extends Model
+{
+    //
+    /**
+     * @var string
+     */
+    protected $table = 'dcmodels';
+
+    /**
+     *
+     * @return string
+     */
+}
