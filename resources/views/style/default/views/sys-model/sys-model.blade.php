2c0bbce14cdc91244a4c754d9d2cf28f6322f57a
cdsuperstar <181977814@qq.com>
2015-08-25 11:12:56 +0800

    push add for  homer, modify

--- resources/views/style/default/views/sys-model/sys-model.blade.php
+++ resources/views/style/default/views/sys-model/sys-model.blade.php
@@ -1,303 +1,139 @@
-
 <!-- BEGIN MAIN CONTENT -->
-<!-- BEGIN start search CONTENT -->
-<div class="portlet box grey-cascade">
-    <div class="portlet-title">
-        <div class="caption">
-            <i class="fa fa-search-plus"></i>查询
-        </div>
-        <div class="tools">
-            <a href="javascript:;" class="collapse">
-            </a>
-        </div>
-    </div>
-    <div class="portlet-body">
-        <table>
-            <tr>
-                <td>模块名称 : &nbsp;</td>
-                <td><input type="text" class="form-control" placeholder="请输入模块名"></td>
-                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button class="btn btn-default green-meadow">
-                    <i class="fa fa-search"></i> &nbsp;查询 </button>
-                </td>
-            </tr>
-        </table>
-    </div>
-</div>
-<!-- END start search CONTENT -->
-
-<div class="row">
-    <div class="col-md-12">
-        <!-- BEGIN EXAMPLE TABLE PORTLET-->
-        <div class="portlet box yellow">
+<div class="row" ng-controller="pipeCtrl as mc">
+    <div class="col-md-12" st-pipe="mc.callServer"  st-table="mc.displayed">
+        <!-- BEGIN start search CONTENT -->
+        <div class="portlet box blue">
             <div class="portlet-title">
                 <div class="caption">
-                    <i class="fa fa-globe"></i>模块管理
+                    <i class="fa fa-search-plus"></i>查询
                 </div>
                 <div class="tools">
-                    <a href="javascript:;" class="reload">
-                    </a>
                     <a href="javascript:;" class="collapse">
                     </a>
                 </div>
             </div>
-            <div class="portlet-body">
-                <div class="table-toolbar">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="btn-group">
-                                <a href="#/sys-addmodel.html">
-                                <button class="btn red-pink">
-                                    添加新模块 <i class="fa fa-plus"></i>
-                                </button>
-                                </a>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="btn-group pull-right">
-                                <button class="btn dropdown-toggle" data-toggle="dropdown">Tools <i class="fa fa-angle-down"></i>
-                                </button>
-                                <ul class="dropdown-menu pull-right">
-                                    <li>
-                                        <a href="javascript:;">
-                                            修改 </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                            删除 </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:;">
-                                            Export to Excel </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
+            <div class="portlet-body" style="overflow: auto;">
+                <div class="col-md-8">
+                    <label>
+                        模块ID: &nbsp; <input st-search="name" st-input-event="blur" placeholder="请输入模块ID" class="input-sm input-inline form-control" type="search"/>
+                    </label>
                 </div>
-                <table class="table table-striped table-bordered table-hover" id="sys-model-table">
+                <div class="col-md-4">
+                    <label>
+                        查询全部 : &nbsp;<input st-search st-input-event="blur" placeholder="请输入查找的信息" class="input-sm input-inline form-control" type="search"/>
+                    </label>
+                </div>
+            </div>
+        </div>
+        <!-- END start search CONTENT -->
+        <!-- BEGIN EXAMPLE TABLE PORTLET-->
+        <div class="portlet box yellow">
+            <div class="portlet-title">
+                <div class="caption">
+                    <i class="fa fa-user"></i>模块管理
+                </div>
+                <div class="tools">
+                    <button class="btn btn-default yellow" onclick="JavaScript:window.location.href='#/sys-addmodel.html';">
+                        <i class="fa fa-plus"></i> &nbsp;添加 </button>
+                    <a href="javascript:;" class="collapse">
+                    </a>
+                </div>
+            </div>
+            <div class="portlet-body" style="overflow: auto;">
+                <div class="col-md-12">
+                    <label>
+                        显示 <input onkeyup="this.value=this.value.replace(/\D/g,'')"  onafterpaste="this.value=this.value.replace(/\D/g,'')" class="input-xsmall input-inline form-control ng-valid ng-dirty ng-valid-number ng-touched" name="items" id="items" type="number" ng-model="itemsByPage" ng-model-options="{ updateOn: 'blur' }" /> 条
+                    </label>
+                </div>
+                <table class="table table-striped table-hover table-bordered">
                     <thead>
                     <tr>
-                        <th class="table-checkbox">
-                            <input type="checkbox" class="group-checkable" data-set="#sys-model-table .checkboxes"/>
-                        </th>
                         <th>
+                            序
+                        </th>
+                        <th st-sort="name">
                             模块ID
                         </th>
-                        <th>
-                            模块图标
-                        </th>
-                        <th>
+                        <th st-sort='title'>
                             模块标题
                         </th>
-                        <th>
-                            父节点
+                        <th st-sort='icon'>
+                            模块图标
+                        </th>
+                        <th st-sort='ismenu'>
+                            是否显示
+                        </th>
+                        <th st-sort='url'>
+                            模块RUL
+                        </th>
+                        <th st-sort='templateUrl'>
+                                模块TemplateURL
                         </th>
                         <th>
-                            模块顺序
+                            编辑
                         </th>
                         <th>
-                            模块Ctrol
-                        </th>
-                        <th>
-                            组件ID
+                            删除
                         </th>
                     </tr>
                     </thead>
                     <tbody>
-                    <tr class="odd gradeX">
-                        <td>
-                            <input type="checkbox" class="checkboxes" value="1"/>
-                        </td>
-                        <td>
-                            模块ID
-                        </td>
-                        <td>
-                            模块图标
-                        </td>
-                        <td>
-                            模块标题
-                        </td>
-                        <td>
-                            父节点
-                        </td>
-                        <td>
-                            模块顺序
-                        </td>
-                        <td>
-                            模块Ctrol
-                        </td>
-                        <td>
-                            组件ID
-                        </td>
+                    <tr ng-show="mc.isLoading">
+                        <td colspan="9" ng-show="mc.isLoading" class="text-center"> 数据加载中 ... </td>
                     </tr>
-                    <tr class="odd gradeX">
-                        <td>
-                            <input type="checkbox" class="checkboxes" value="1"/>
-                        </td>
-                        <td>
-                            模块ID
-                        </td>
-                        <td>
-                            模块图标
-                        </td>
-                        <td>
-                            模块标题
-                        </td>
-                        <td>
-                            父节点
-                        </td>
-                        <td>
-                            模块顺序
-                        </td>
-                        <td>
-                            模块Ctrol
-                        </td>
-                        <td>
-                            组件ID
-                        </td>
+                    <tr ng-show="mc.noResult">
+                        <td colspan="9" ng-show="mc.noResult" class="text-center" bgcolor="#f0f8ff"> 无符合条件数据! 请输入条件重新查询...  </td>
                     </tr>
-                    <tr class="odd gradeX">
+                    <tr ng-repeat="user in mc.displayed">
+                        <td align="center">@{{ $index+1 }}</td>
                         <td>
-                            <input type="checkbox" class="checkboxes" value="1"/>
+                            @{{ user.name }}
                         </td>
                         <td>
-                            模块ID
+                            @{{user.title}}
                         </td>
                         <td>
-                            模块图标
+                            @{{user.icon}}
                         </td>
                         <td>
-                            模块标题
+                            @{{user.ismenu}}
                         </td>
                         <td>
-                            父节点
+                            @{{user.url}}
                         </td>
                         <td>
-                            模块顺序
+                            @{{user.templateUrl}}
                         </td>
-                        <td>
-                            模块Ctrol
+                        <td align="center">
+                            <a href="javascript:;" class="dcaedit" ng-click="edituser(user)" title="编辑该条数据！">
+                                Edit <i class="fa fa-edit"></i>
+                            </a>
                         </td>
-                        <td>
-                            组件ID
-                        </td>
-                    </tr>
-                    <tr class="odd gradeX">
-                        <td>
-                            <input type="checkbox" class="checkboxes" value="1"/>
-                        </td>
-                        <td>
-                            模块ID
-                        </td>
-                        <td>
-                            模块图标
-                        </td>
-                        <td>
-                            模块标题
-                        </td>
-                        <td>
-                            父节点
-                        </td>
-                        <td>
-                            模块顺序
-                        </td>
-                        <td>
-                            模块Ctrol
-                        </td>
-                        <td>
-                            组件ID
-                        </td>
-                    </tr>
-                    <tr class="odd gradeX">
-                        <td>
-                            <input type="checkbox" class="checkboxes" value="1"/>
-                        </td>
-                        <td>
-                            模块ID
-                        </td>
-                        <td>
-                            模块图标
-                        </td>
-                        <td>
-                            模块标题
-                        </td>
-                        <td>
-                            父节点
-                        </td>
-                        <td>
-                            模块顺序
-                        </td>
-                        <td>
-                            模块Ctrol
-                        </td>
-                        <td>
-                            组件ID
-                        </td>
-                    </tr>
-                    <tr class="odd gradeX">
-                        <td>
-                            <input type="checkbox" class="checkboxes" value="1"/>
-                        </td>
-                        <td>
-                            模块ID
-                        </td>
-                        <td>
-                            模块图标
-                        </td>
-                        <td>
-                            模块标题
-                        </td>
-                        <td>
-                            父节点
-                        </td>
-                        <td>
-                            模块顺序
-                        </td>
-                        <td>
-                            模块Ctrol
-                        </td>
-                        <td>
-                            组件ID
-                        </td>
-                    </tr>
-                    <tr class="odd gradeX">
-                        <td>
-                            <input type="checkbox" class="checkboxes" value="1"/>
-                        </td>
-                        <td>
-                            模块ID
-                        </td>
-                        <td>
-                            模块图标
-                        </td>
-                        <td>
-                            模块标题
-                        </td>
-                        <td>
-                            父节点
-                        </td>
-                        <td>
-                            模块顺序
-                        </td>
-                        <td>
-                            模块Ctrol
-                        </td>
-                        <td>
-                            组件ID
+                        <td align="center">
+                            <a href="javascript:;" class="dcadel" ng-click="deluser(user)" confirmation-needed="确定要删除该条数据吗？" title="删除该条数据！">
+                                Delete <i class="fa fa-times"></i>
+                            </a>
                         </td>
                     </tr>
                     </tbody>
+                    <tfoot>
+                    <tr>
+                        <td colspan="9"  st-pagination st-items-by-page="itemsByPage" class="text-center">
+                        </td>
+                    </tr>
+                    </tfoot>
                 </table>
+
             </div>
         </div>
         <!-- END EXAMPLE TABLE PORTLET-->
     </div>
 </div>
 
+
 <!-- END MAIN CONTENT -->
 <!-- BEGIN MAIN JS & CSS -->
 <script language="JavaScript" type="text/javascript">
-    TableManaged.init();
+    //    TableEditable.init();
 </script>
 <!-- BEGIN MAIN JS & CSS -->
