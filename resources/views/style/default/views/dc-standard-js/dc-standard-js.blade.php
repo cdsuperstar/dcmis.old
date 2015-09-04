eb72374ab28eedddb7e88edef77fccb6ccc28dc9
cdsuperstar <181977814@qq.com>
2015-09-04 17:37:41 +0800

    push add for  homer, modify

---
+++ resources/views/style/default/views/dc-standard-js/dc-standard-js.blade.php
@@ -0,0 +1,11 @@
+
+<!-- BEGIN MAIN CONTENT -->
+
+
+<!-- END MAIN CONTENT -->
+<!-- BEGIN MAIN JS & CSS -->
+<script language="JavaScript" type="text/javascript">
+// BEGIN INIT SCRIPT
+// END INIT SCRIPT
+</script>
+<!-- BEGIN MAIN JS & CSS -->
